547896f82ef2a9cbe21b34506a369b0097ac8e3d
Matviishyna Julia <s2100159@edu.bc.fi>
2021-03-17 14:26:09 +0200

    registration of players, choose colors

--- js/kimble.js
+++ js/kimble.js
@@ -94,29 +94,34 @@ let yellowpices = document.querySelectorAll(".yellowpiece");
 let greenpices = document.querySelectorAll(".greenpiece");
 let kimblestartbutton = document.querySelector("button");
 let result = document.getElementById("result");
+let form = document.querySelector("form");
+let turns = document.getElementById("turns");
 
 const ShowPopupplayers = () => {
   document.getElementById("popup-players").style.display = "block";
 };
 document.onload = ShowPopupplayers();
 
-const GetPlayers = (event) => {
-  event.preventDefault();
+const GetPlayers = () => {
   player1 = {
     nickname: `${document.getElementById("player1").value}`,
     color: document.getElementById("color1").value,
+    arrowturn: document.getElementById("color1").value + "turn",
   };
   player2 = {
     nickname: document.getElementById("player2").value,
     color: document.getElementById("color2").value,
+    arrowturn: document.getElementById("color1").value + "turn",
   };
   player3 = {
     nickname: document.getElementById("player3").value,
     color: document.getElementById("color3").value,
+    arrowturn: document.getElementById("color1").value + "turn",
   };
   player4 = {
     nickname: document.getElementById("player4").value,
     color: document.getElementById("color4").value,
+    arrowturn: document.getElementById("color1").value + "turn",
   };
   console.log(player1, player2);
   let allplayers = [];
@@ -127,18 +132,32 @@ const GetPlayers = (event) => {
   });
 
   console.log(activeplayers);
-  activeplayers.forEach((el) => {
-    if ((el.color = (el - 1).color)) {
-      console.log("duplicated item " + el.color);
-    }
+
+  /*check colors*/
+  let checkcolor = activeplayers.map(function (item) {
+    return item.color;
   });
-
-  if (activeplayers.length < 2) {
-    result.textContent = `You need minimum 2 players`;
+  let isDuplicate = checkcolor.some(function (item, idx) {
+    return checkcolor.indexOf(item) != idx;
+  });
+  if (isDuplicate) {
+    console.log(
+      "Items are duplicated and here we need popup HTML CSS and function to put pieces and players on the field"
+    );
+  } else {
+    document.getElementById("popup-players").style.display = "none";
+    turns.classList.add(player1.arrowturn);
   }
-
-  /* document.getElementById("popup-players").style.display = "none"; */
 };
-kimblestartbutton.addEventListener("click", GetPlayers);
+const StopRefresh = (event) => {
+  event.preventDefault();
+  GetPlayers();
+};
 
-/*colors of pieces*/
+form.addEventListener("submit", StopRefresh);
+
+/*Game Start*/
+
+/*2 moves on 6 dice*/
+/* if (dicenumber>0) {
+} */
